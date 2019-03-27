338d05ab94db2fd2a83d684340a97cbbdf6d7712
wlidemberg <automatizei@gmail.com>
2019-03-27 13:22:53 -0300

    Criamos a Classe Page

---
+++ views-cache/header.003873c670aac278cd0a8e0d7f83d824.rtpl.php
@@ -0,0 +1,9 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Hcode Store</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        
