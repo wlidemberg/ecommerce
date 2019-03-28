13ff3083e8918f484f8f0e7e5abf1107cb39114f
wlidemberg <automatizei@gmail.com>
2019-03-28 13:59:46 -0300

    criando o PageAdmin e tpl admin

--- index.php
+++ index.php
@@ -4,15 +4,24 @@ require_once("vendor/autoload.php");
 
 use \Slin\Slin;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
+/***** ROTAS DA VIEW *****/
 $app->get('/', function() {
     
 	$page = new Page();
         $page->setTpl("index");
+});
+
+/***** ROTAS DA VIEW/ADMIN *****/
+$app->get('/admin', function(){
+    
+    $page = new PageAdmin();
+    $page->setTpl('index');
 });
 
 $app->run();
